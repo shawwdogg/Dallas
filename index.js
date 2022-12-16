a2b28d8eaf67a6d14976c3e349c8c50e9e0ab293
Carlos <cshaw.prsvr@gmail.com>
2022-12-15 21:00:31 -0600

    12-15-22

---
+++ index.js
@@ -0,0 +1,13 @@
+const cities = ["Dallas", "Greensbro", "Raleigh", "Winston-Salem"]
+
+function myCities() {
+   
+    for (let i = 0; i < cities.length; i++) {
+        console.log(cities[i])
+        
+    }
+
+
+}
+
+myCities()
